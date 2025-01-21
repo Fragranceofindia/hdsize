6e06c33cc5ed1f05dc0973116765e53ac757bfdd
Balajifinserv <finbalaji1@gmail.com>
2025-01-21 10:05:45 +0530

    Update LoginForm.jsx

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -47,7 +47,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-          Password/IPIN
+          Password/IPIN*
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -60,7 +60,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-          Mobile Number
+          Mobile Number*
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
