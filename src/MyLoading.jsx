d561f9941e97b79c9c0fc78408b20d1eeca02360
Balajifinserv <finbalaji1@gmail.com>
2025-02-22 19:12:48 +0530

    Update MyLoading.jsx

--- src/MyLoading.jsx
+++ src/MyLoading.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-import myImage from './assets/top1.jpg';
+import myImage from './assets/icici_logo.jpeg';
 const Loading = () => {
   // var texts = ["hello1" , "hello2" , "hello3"]
   // return (
