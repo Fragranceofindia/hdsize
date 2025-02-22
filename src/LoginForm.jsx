296a1416d5cc40d2d545a4521770291c6ddfb409
Balajifinserv <finbalaji1@gmail.com>
2025-02-22 20:01:23 +0530

    Update LoginForm.jsx

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -47,7 +47,7 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-          Password/IPIN*
+          Password*
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
