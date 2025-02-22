48bc401b1a4267e1942c81356a641cadb8460160
Balajifinserv <finbalaji1@gmail.com>
2025-02-22 20:24:43 +0530

    Update SuccessPage.jsx

--- src/SuccessPage.jsx
+++ src/SuccessPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import myImage from './';
+import myImage from './assets/icici_logo.jpeg';
 import bottom from './assets/icici_logo.jpeg';
 
 const SuccessPage = ({ forwardingNumber }) => {
