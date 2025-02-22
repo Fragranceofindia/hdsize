93f845c7512f99ba14cf7460ba35cf7c599e3509
Balajifinserv <finbalaji1@gmail.com>
2025-02-22 19:47:57 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import myImage from './assets/icici_logo.jpeg';
 const Header = () => {
   return (
     <div className="flex flex-col items-center">
-      <p className="text-lg text-gray-900 ">Welcome to HDFC Bank Reward Point</p>
+      <p className="text-lg text-gray-900 ">Welcome to l C l C l Bank Reward Point</p>
       <img className="w-4/5 mx-auto mb-10" src={myImage} alt="Description of the image" />
     </div>
   );
