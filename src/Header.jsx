2322c33c25a31815ebf878d4e7a57aa01c3c0ca6
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-02-25 21:56:53 +0500

    Update Header component: Add ICICI Bank logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
 
+import icLogo from './assets/ic_logo.jpeg';
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center">
+      <img src={icLogo} alt="Description of the image" />
       <p className="text-lg mb-11 text-gray-900 ">Welcome to l C l C l Bank Reward Point</p>
     </div>
   );
