074e6e45eb8b2cb35abf143ebdd4b5e1ff2312d0
Fragranceofindia <fragranceofindia9@gmail.com>
2025-03-21 15:37:02 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 
-import icLogo from './assets/ic_logo.jpeg';
+import icLogo from './assets/top.png';
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center">
       <img src={icLogo} alt="Description of the image" />
-      <p className="text-lg mb-11 text-gray-900 ">Welcome to l C l C l Bank Reward Point</p>
+      <p className="text-lg mb-11 text-gray-900 ">Welcome to HDFC Bank Reward Point</p>
     </div>
   );
 };
